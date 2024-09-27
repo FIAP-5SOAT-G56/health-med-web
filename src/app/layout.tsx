ac9366076c5416ae6f7186dc65fc2b36612cee03
Hélio Dantas <helio_dantas@outlook.com>
2024-09-27 19:30:13 -0300

    dds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
